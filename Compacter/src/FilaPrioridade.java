2f328decaa8593a828f31c341cc5a4197e5c2990
Tiago Ary Castelo <tiago.ary10@gmail.com>
2021-06-14 18:09:04 -0300

    Finalização No + Fila Prioridade

--- Compacter/src/FilaPrioridade.java
+++ Compacter/src/FilaPrioridade.java
@@ -1,2 +1,81 @@
 public class FilaPrioridade {
+    private No primeiro;
+    private No ultimo;
+    private int contador;
+
+    public FilaPrioridade() {
+        primeiro = null;
+        ultimo = null;
+        contador = 0;
+    }
+
+    public void enqueue(String valor, int prioridade) {
+        No novo = new No(valor,prioridade);
+        int flag = 0;
+        if (primeiro == null) {  // Lista vazia?
+            primeiro = novo;
+            ultimo = novo;
+
+        }
+        else if (novo.prioridade < primeiro.prioridade){
+            novo.proximo = primeiro;
+            primeiro.anterior = novo;
+            primeiro = novo;
+
+        }
+        else if (novo.prioridade >= ultimo.prioridade) {
+            ultimo.proximo = novo;
+            novo.anterior = ultimo;
+            ultimo = novo;
+        }
+        else{
+            No aux = primeiro;
+
+            while (aux != null && novo.prioridade >= aux.prioridade){
+                aux = aux.proximo;
+            }
+            novo.proximo = aux;
+            novo.anterior = aux.anterior;
+            aux.anterior = novo;
+            novo.anterior.proximo = novo;
+
+
+        }
+        contador++;
+
+    }
+    public void dequeue() {
+        if(primeiro != null) {
+            primeiro = primeiro.proximo;
+            contador--;
+        }
+    }
+
+    public No front() {
+        if(primeiro != null) {
+            return primeiro;
+        }
+        return null;
+
+    }
+
+    public boolean empty(){
+        return contador == 0;
+    }
+
+
+
+    public int size() {
+        return contador;
+    }
+
+    public void exibir() {
+        No aux = primeiro;
+
+        while (aux != null) {
+            System.out.println(aux.dado);
+
+            aux = aux.proximo;
+        }
+    }
 }
--- Compacter/src/No.java
+++ Compacter/src/No.java
@@ -1,8 +1,24 @@
 public class No {
+
+
+
     public No Esquerdo;
     public No Direito;
-    public String value;
+    public String dado;
     public int prioridade;
-    public No Anterior;
+    public No anterior;
     public No proximo;
+
+    public No(String dado, int prioridade){
+        this.prioridade = prioridade;
+        this.dado = dado;
+    }
+    public No(String dado, int prioridade, No Esquerdo, No Direito, No Anterior, No Proximo){
+        this.prioridade = prioridade;
+        this.dado = dado;
+        this.anterior = Anterior;
+        this.proximo = Proximo;
+        this.Esquerdo = Esquerdo;
+        this.Direito = Direito;
+    }
 }
